9bcbe534c4092088a53f426ba6b85b98808f2591
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2025-12-10 09:18:09 +0300

    add images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+
+  static const String imagesForgotPassword =
+      "assets/images/forgot_password.svg";
+
+  static const String imagesOnbording1 = "assets/images/onbording_1.svg";
+
+  static const String imagesOnbording2 = "assets/images/onbording_2.svg";
+
+  static const String imagesOnbording3 = "assets/images/onbording_3.svg";
+
+  static const String imagesSignIn = "assets/images/sign_in.svg";
+
+  static const String imagesSplash = "assets/images/splash.svg";
+
+  static const String imagesSucccesOrder = "assets/images/succces_order.svg";
+}
