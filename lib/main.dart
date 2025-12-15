7d4c417e1cdaa8e05a3fe70625448e350274909b
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2025-12-15 13:08:47 +0300

    added firebase for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'core/routes/app_pages.dart';
@@ -5,10 +6,12 @@ import 'core/routes/app_routes.dart';
 import 'core/services/app_services.dart';
 import 'core/translation/app_translations.dart';
 import 'core/utils/app_colors.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialServices();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const DalelAPP());
 }
 
